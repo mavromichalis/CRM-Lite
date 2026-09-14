e29065d5d4fb46ee5b1fd2d977eab059632055a4
Panos <panossmav@icloud.com>
2026-09-15 00:27:13 +0300

    Add order creation

---
+++ services/orders.py
@@ -0,0 +1,31 @@
+from ..db.connection import connect_db
+from ..models.order import Order
+from ..utils.id_generator import generate_id
+
+def create_order(status,customer_id,products):
+    conn , cur = connect_db()
+    id = generate_id('orders')
+    try:
+        price = 0 
+        for product in products:
+            cur.execute(
+                """
+                SELECT price FROM products WHERE id = %s
+                """
+            ,(product))
+            res = cur.fetchone()
+            if res is None:
+                raise Exception
+            price+=res
+        cur.execute(
+            """
+            INSERT INTO orders (id,status,customer_id,products,price) VALUES (%s,%s,%s,%s,%s)
+            """
+        ,(id,status,customer_id,products,price))
+        conn.commit()
+        return Order(id,status,customer_id,products,price)
+    except Exception:
+        conn.rollback()
+        return None
+    finally:
+        conn.close()
