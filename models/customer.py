865e0d11c0ea361390b27796279b965cadda57fb
Panos <panossmav@icloud.com>
2026-09-14 14:39:54 +0300

    Continue customer

--- models/customer.py
+++ models/customer.py
@@ -16,7 +16,35 @@ class Customer:
         self.last_modified =  last_modified
         self.status = status
 
-    #TODO def add_order(self,order_no):
+    def add_order(self,order_no):
+        conn , cur = connect_db()
+        self.orders.append(order_no)
+        try:
+            cur.execute(
+                """
+                UPDATE customers SET orders = %s WHERE id = %s 
+                """
+            ,(self.orders,self.id))
+            conn.commit()
+        except:
+            conn.rollback()
+            self.orders.remove(order_no)
+        finally:
+            conn.close()
+
+    def get_customer_orders(self):
+        conn , cur = connect_db()
+        try:
+            cur.execute(
+                """
+                SELECT * FROM orders WHERE customer_id = %s
+                """
+            ,(self.id,))
+            return cur.fetchall()
+        finally:
+            conn.close()
+    
+
 
     def change_status(self,new_status):
         conn , cur = connect_db()
